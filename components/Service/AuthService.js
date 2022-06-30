0779d533b05436227445bcc4a8d313bea0ecd354
wishtree-vpatel <v.patel@wishtreetech.com>
2022-06-30 10:51:22 +0530

    Add Forgot password and Reset Password Api call funcation

--- components/Service/AuthService.js
+++ components/Service/AuthService.js
@@ -5,6 +5,12 @@ class AuthService {
         return http.post(`/call-login/`,data);
     }
     
+    callForgotPassword(data){
+        return http.post(`/forgot-password/`,data);
+    }
     
+    callResetPassword(data,id){
+        return http.post(`/reset-password/`+id,data);
+    }
   }
   export default new AuthService();
